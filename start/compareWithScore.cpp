e91c78ddcceb882c21e72dabdde12cbc8ac6a74f
ll06printf <ll06nest@outlook.com>
2024-07-02 11:28:10 +0800

    重置
    On branch main
    Changes to be committed:
    	deleted:    main.cpp
    	deleted:    start/compareWithName.cpp
    	deleted:    start/compareWithScore.cpp
    	deleted:    start/del.cpp
    	deleted:    start/displayEntry.cpp
    	deleted:    start/findWithId.cpp
    	deleted:    start/input.cpp
    	deleted:    start/inputEntry.cpp
    	deleted:    start/interface.h
    	deleted:    start/menu.h
    	deleted:    start/modify.cpp
    	deleted:    start/output.cpp
    	deleted:    start/primitive.h
    	deleted:    start/read_from_file.cpp
    	deleted:    start/search.cpp
    	deleted:    start/sortArr.cpp
    	deleted:    start/sort_na.cpp
    	deleted:    start/sort_nd.cpp
    	deleted:    start/sort_sa.cpp
    	deleted:    start/sort_sd.cpp
    	deleted:    start/write_to_file.cpp

--- start/compareWithScore.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by LL06p on 24-7-1.
-//
-#include "interface.h"
-#include "primitive.h"
-
-int compareWithScore(const struct student *lhs, const struct student *rhs) {
-    return lhs->sum - rhs->sum;
-}
